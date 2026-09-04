fcc82d6dc786b2ba969be58b24c8ccad9fe4129a
Susan Liu <susan.liu53@student.qcc.cuny.edu>
2026-09-04 16:25:21 +0000

    END OF CLASS; FINISH NOTES AFTER

---
+++ NOTES/09_04_notes.cpp
@@ -0,0 +1,57 @@
+/*
+********basics to re-learn********
+PASS by REFERENCE (&): uses the address operator --> memory address
+    - using the direct address of the variable will allow you to change that variable
+        - less safe b/c OG is modified 
+    - uses less memory b/c it uses the direct instead of creating more data -> MORE memroy uses
+PASS by VALUE: creates a COPY of variable INSTEAD OF USING OG
+    - doesn't change OG
+        - safer since its a copy
+    - creates more data -> more memory
+
+LINEAR: Elements w/in the structure are arranged sequentially in ONE seuquence
+
+
+NON-LINEAR: Elements w/in structure are NOT in ONE SEQUENCE
+BINARY TREE
+- splits by 2 nodes that continuously split; LT < the parent node & RT node > parent node
+GRAPH
+HEAP
+
+CONVERSION
+RECURSIONS: 
+LINKED DATA STRUCTURES: data structures where nodes are connected using pointers
+    - Node: variable that stores data
+    - Pointers: link between each nodes
+ABSTRACT DATA STRUCTURE: data structure that runs w/o explicit boundaries of HOW
+CONCRETE DATA STRUCTURES: 
+DYNAMIC DATA STRUCTURE:
+GRAPH
+    - shows you the connections of each vertices
+
+STACK
+    - last in, but is the first out
+QUEUE
+    - first in AND first out
+LIST
+- search through one by one
+    - LINKED LIST
+        -Doubly Linked: can be searched/move from both the front and end 
+            - could move faster than singly
+            - more memory because you need to be able to create the algo to function forward and then another to backwards
+        -Singly Linekd: can only move from front to back or vice versa
+            - less memory b/c it is simpler
+            - can be converted to a Doubly BUT it could become constant
+
+    - 
+ARRAY
+    -static: fixed-value that you set 
+        - 
+    -dynamic: able to be expanded/shrunk 
+        - 
+
+BINARY TREE
+- splits by 2 nodes that continuously split; LT < the parent node & RT node > parent node
+
+
+*/
